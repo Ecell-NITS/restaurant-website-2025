654532e49e4fd197dffd477d70c1611cf81d0cdf
swastika-paul <paulswastika267@gmail.com>
2025-11-01 00:08:18 +0530

    added new menu page

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,35 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Home from './pages/Home/home';
-import Menu from './pages/Menu/menu';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home/home";
+import Menu from "./pages/Menu/menu";
+import "./App.css";
 
 function App() {
   return (
     <Router>
-      <header className="p-4 bg-white shadow">
-        <nav className="container mx-auto flex gap-4">
-          <Link to="/" className="text-blue-600 hover:underline">
-            Home
-          </Link>
-          <Link to="/menu" className="text-blue-600 hover:underline">
-            Menu
-          </Link>
+      {/* NAVBAR */}
+      <header className="navbar">
+        {/* Left side: Logo */}
+        <div className="logo-container">
+          <img src="/logo.jpg" alt="Restaurant Logo" className="nav-logo" />
+          <h1 className="brand-name">Bon Ton</h1>
+        </div>
+
+        {/* Center: Navigation Links */}
+        <nav className="nav-links">
+          <Link to="/">Home</Link>
+          <Link to="/menu">Menu</Link>
+          <Link to="/visit">Visit Us</Link>
+          <Link to="/services">Services</Link>
+          <Link to="/review">Review</Link>
         </nav>
+
+        {/* Right side: Button */}
+        <button className="book-btn">Book a Table</button>
       </header>
 
-      <main>
+      {/* MAIN PAGE CONTENT */}
+      <main className="main-section">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/menu" element={<Menu />} />
